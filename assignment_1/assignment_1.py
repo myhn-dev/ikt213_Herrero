86f7858a758f1b3cbad1a7c973e7d5878a4d7310
Lucas Herrero <leherreror@uia.no>
2026-08-30 05:08:58 +0200

    Updated python script to save camera outputs and save it in /solutions .

--- assignment_1/assignment_1.py
+++ assignment_1/assignment_1.py
@@ -9,14 +9,29 @@ def print_image_information(image):
     print("Size:", image.size)
     print("Data Type:", image.dtype)
 
+def save_camera_information():
+    camera = cv2.VideoCapture(0)
+
+    fps = camera.get(cv2.CAP_PROP_FPS)
+    height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+    with open("solutions/camera_outputs.txt", "w") as file:
+        file.write(f"fps: {fps}\n")
+        file.write(f"height: {height}\n")
+        file.write(f"width: {width}\n")
+
+    camera.release()
+
 def main():
-        image = cv2.imread("iris-1.jpg")
+    image = cv2.imread("iris-1.jpg")
 
-        if image is None:
-            print("Could not load image")
-            return
+    if image is None:
+        print("Could not load image")
+        return
 
-        print_image_information(image)
+    print_image_information(image)
+    save_camera_information()
 
 if __name__ == "__main__":
     main()
