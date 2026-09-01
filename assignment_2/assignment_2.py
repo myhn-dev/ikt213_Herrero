3623c92562c27b7fdb9455bb19b1daef376ea64e
Lucas Herrero <leherreror@uia.no>
2026-09-01 21:58:37 +0200

    9/9 functions complete and images saved /solutions

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -32,27 +32,43 @@ def manual_copy(image, emptyPictureArray=None):
     for y in range(height):
         for x in range(width):
             emptyPictureArray[y, x] = image[y, x]
-
     return emptyPictureArray
 
 def grayscale(image):
-    # ...
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return grayscale_image
 
 def hsv(image):
-    # ...
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     return hsv_image
 
-def hue_shifted(image, emptyPictureArray, hue):
-    # ...
-    return hue_shifted_image
+def hue_shifted(image, emptyPictureArray=None, hue=50):
+    heigth, width, channels = image.shape
+
+    if emptyPictureArray is None:
+        emptyPictureArray = np.zeros((heigth, width, 3), dtype=np.uint8)
+
+    for y in range(heigth):
+        for x in range(width):
+            for channel in range(channels):
+                emptyPictureArray[y, x, channel] = (
+                    int(image[y, x, channel]) + hue
+                ) % 256
+                #to stay within the 0-255 range
+
+    return emptyPictureArray
 
 def smoothing(image):
-    # ...
+    smoothed_image = cv2.GaussianBlur(image, (15, 15), 0, borderType=cv2.BORDER_DEFAULT)
     return smoothed_image
 
 def rotation(image, rotation_angle):
-    # ...
+    if rotation_angle == 90:
+        rotated_image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+
+    elif rotation_angle == 180:
+        rotated_image = cv2.rotate(image, cv2.ROTATE_180)
+
     return rotated_image
 
 
@@ -75,7 +91,20 @@ def main():
     copied_image = manual_copy(image)
     cv2.imwrite("solutions/manual_copied_image.jpg", copied_image)
 
+    grayscale_image = grayscale(image)
+    cv2.imwrite("solutions/grayscale_image.jpg", grayscale_image)
 
+    hsv_image = hsv(image)
+    cv2.imwrite("solutions/hsv_image.jpg", hsv_image)
+
+    hue_shifted_image = hue_shifted(image, hue=50)
+    cv2.imwrite("solutions/hue_shifted_image.jpg", hue_shifted_image)
+
+    smoothed_image = smoothing(image)
+    cv2.imwrite("solutions/smoothed_image.jpg", smoothed_image)
+
+    rotated_image = rotation(image, 180)
+    cv2.imwrite("solutions/rotated_image.jpg", rotated_image)
 
 
 if __name__ == "__main__":
